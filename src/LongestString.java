9a5d9eb9e44356abb1d3b15b28021513d7e44d08
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2018-09-02 21:34:05 +0200

    commit with charArr

--- src/LongestString.java
+++ src/LongestString.java
@@ -2,15 +2,16 @@ public class LongestString {
 
     public static void main (String [] args){
 
-        String string = "adddkkkkrrrrrrr                bbbbbbbbyyyyyy";
+        String string = "adddkkkkjjjjjjjjjj      yyyyyy";
         System.out.println(longestString(string));
     }
 
     public static int longestString(String string){
         int maxSize = 1;
         int currSize = 1;
-        for (int i = 1; i < string.length(); i++) {
-            if(string.substring(i-1,i).equals(string.substring(i,i+1))){
+        char charArr [] = string.toCharArray();
+        for (int i = 1; i < charArr.length; i++) {
+            if(charArr[i-1] == charArr[i]){
                 currSize ++;
                 if (currSize > maxSize){
                     maxSize = currSize;
